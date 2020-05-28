b7e56242949e815ed693843f48a788d472639b8c
M3LNYK <vikmel19@gmail.com>
2020-05-28 23:47:58 +0200

    Modify GUI to look like draft
    This version of giu is a draft for future versions,
    but still need to be modified.

--- gui.py
+++ gui.py
@@ -2,10 +2,19 @@ from tkinter import *
 
 
 root = Tk()
+root.title("Mvv app")
+root.minsize(300, 400)
 
 mylable = Label(root, text = "Hello there!")
 
+button_choose = Button(root, text = "Choose", bg ="red", fg = "white")
+button_modify = Button(root, text = "Modify", bg ="orange", fg = "white")
+button_run = Button(root, text = "GO!", bg ="green", fg = "white")
+
 # mylable.grid(row = 0, column = 1)
-mylable.pack()
+button_choose.grid(row =0, column = 0)
+button_modify.grid(row =1, column = 0)
+button_run.grid(row =2, column = 0)
+# mylable.pack()
 
 root.mainloop()
