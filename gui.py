c7cebc019d567b5044136a0f11a4fcfc16566ba8
M3LNYK <vikmel19@gmail.com>
2020-05-29 22:02:51 +0200

    Modify GUI buttons to look better
    Changed the size of buttons and modified margins from
    each side.

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+# import main
 from tkinter import *
 
 
@@ -7,14 +8,14 @@ root.minsize(300, 400)
 
 mylable = Label(root, text = "Hello there!")
 
-button_choose = Button(root, text = "Choose", bg ="red", fg = "white")
-button_modify = Button(root, text = "Modify", bg ="orange", fg = "white")
-button_run = Button(root, text = "GO!", bg ="green", fg = "white")
+button_choose = Button(root, text = "Choose", bg ="red", fg = "white", height = 10, width = 40)
+button_modify = Button(root, text = "Modify", bg ="orange", fg = "white", height = 10, width = 40)
+button_run = Button(root, text = "GO!", bg ="green", fg = "white", height = 10, width = 38)
 
 # mylable.grid(row = 0, column = 1)
-button_choose.grid(row =0, column = 0)
-button_modify.grid(row =1, column = 0)
-button_run.grid(row =2, column = 0)
+button_choose.grid(row =0, column = 0, pady = 10, padx = 5)
+button_modify.grid(row =1, column = 0, pady = 10, padx = 5)
+button_run.grid(row =2, column = 0, pady = 10, padx = 5)
 # mylable.pack()
 
 root.mainloop()
