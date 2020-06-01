e78caa6af74b6d5f8659e3773a9a370ea1650987
M3LNYK <vikmel19@gmail.com>
2020-06-01 21:40:41 +0200

    Add state checker for green button
    Yellow and red buttons influence green button, if there is
    no destination and location of files - green button is not
    active. If states of flags are changed to true, green
    button wil become available.

--- gui.py
+++ gui.py
@@ -8,24 +8,47 @@ root = Tk()
 root.title("Mvv app")
 root.minsize(300, 400)
 
+global checker1, checker2
+checker1 = FALSE
+checker2 = FALSE
 
 def opener():
-    global task_folder
     # For choosing directory
+    global task_folder
     task_folder = root.directory = askdirectory()     # return folder location
     # Label(root, text = root.directory).grid(row = 4, column = 0)
-    # main_one(root.directory)
-    print(task_folder)
+    global checker1
+    checker1 = TRUE
+    tester()
 
 
 def creator():
     global destination_folder
+    global checker2
     destination_folder = root.directory = askdirectory()
+    checker2 = TRUE
+    tester()
+
+def tester():
+    if checker1 == TRUE and checker2 == TRUE:
+        print("GG, WP")
+        button_run = Button(root, text = "GO!", bg = "green", fg = "white",
+                            height = 10, width = 38, state = NORMAL)
+        button_run.grid(row = 2, column = 0, pady = 10, padx = 5)
+    elif checker1 == FALSE and checker2 == TRUE:
+        print("Choose fies to sort!")
+        print(checker1, checker2)
+    elif checker1 == TRUE and checker2 == FALSE:
+        print("Choose where to drop sorted photos!")
+        print(checker1, checker2)
+    elif checker1 == FALSE and checker2 == FALSE:
+        print("Chose files to choose and where to place them!")
 
 
 button_choose = Button(root, text = "Choose", bg ="red", fg = "white", height = 10, command = opener)
 button_modify = Button(root, text = "Modify", bg ="orange", fg = "white", height = 10, width = 40, command = creator)
-button_run = Button(root, text = "GO!", bg ="green", fg = "white", height = 10, width = 38)
+button_run = Button(root, text = "GO!", bg ="green", fg = "white",
+                    height = 10, width = 38, state = DISABLED, disabledforegroun="red")
 
 # mylable.grid(row = 0, column = 1)
 button_choose.grid(row =0, column = 0, pady = 10, padx = 5, sticky = W+E)
