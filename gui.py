e26510e6cf96543af5df58e17e2fbe5019d1c50a
M3LNYK <vikmel19@gmail.com>
2020-06-02 22:54:04 +0200

    Modify gui.py by adding restart button
    gui.py will now have restart button, which resets
    all flags, destination_folder and location_folder.
    
    Look of restart button is not final.

--- gui.py
+++ gui.py
@@ -54,6 +54,41 @@ def tester():
         print("Chose files to choose and where to place them!")
 
 
+def reseter():
+    global checker1
+    checker1 = FALSE
+    global checker2
+    checker2 = FALSE
+    global task_folder
+    task_folder = None
+    global destination_folder
+    destination_folder = None
+    button_choose = Button(root,
+                           text = "Choose folder with files to sort",
+                           bg = "red", fg = "white",
+                           height = 10, command = opener)
+    button_modify = Button(root,
+                           text = "Choose folder where sorted files should be sorted",
+                           bg = "orange", fg = "white",
+                           height = 10, width = 40,
+                           command = creator)
+    button_run = Button(root,
+                        text = "Not ready to start",
+                        bg = "Grey", fg = "white",
+                        height = 10, width = 38,
+                        state = DISABLED,
+                        disabledforegroun = "Black")
+    button_restart = Button(root,
+                            text = "Res",
+                            height = 5, width = 5,
+                            pady = 5, padx = 5,
+                            command = reseter)
+    button_choose.grid(row = 0, column = 0, pady = 10, padx = 5, sticky = W + E)
+    button_modify.grid(row = 1, column = 0, pady = 10, padx = 5)
+    button_run.grid(row = 2, column = 0, pady = 10, padx = 5)
+    button_restart.grid(row = 3, column = 0, pady = 0, padx = 0)
+
+
 button_choose = Button(root,
                        text = "Choose folder with files to sort",
                        bg ="red", fg = "white",
@@ -69,12 +104,18 @@ button_run = Button(root,
                     height = 10, width = 38,
                     state = DISABLED,
                     disabledforegroun="Black")
+button_restart = Button(root,
+                        text = "Res",
+                        height = 5, width = 5,
+                        pady = 5, padx = 5,
+                        command = reseter)
 
 
 # mylable.grid(row = 0, column = 1)
-button_choose.grid(row =0, column = 0, pady = 10, padx = 5, sticky = W+E)
-button_modify.grid(row =1, column = 0, pady = 10, padx = 5)
-button_run.grid(row =2, column = 0, pady = 10, padx = 5)
+button_choose.grid(row = 0, column = 0, pady = 10, padx = 5, sticky = W + E)
+button_modify.grid(row = 1, column = 0, pady = 10, padx = 5)
+button_run.grid(row = 2, column = 0, pady = 10, padx = 5)
+button_restart.grid(row = 3, column = 0, pady = 0, padx = 0)
 # mylable.pack()
 
 root.mainloop()
